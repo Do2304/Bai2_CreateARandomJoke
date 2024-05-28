5e7b10484f230a6171eb33e4d4c34209cd4fe4bb
Luli <lulimanucians198.com>
2024-05-28 16:14:19 +0700

    lam lan 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-function MyButton(){
-  return(
-    <button>TBD</button>
-  );
-}
-export default function MyApp(){
+// import Joke from "./Joke";
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <h1>Joke Generator Using React and Joke API</h1>
+//             <Joke/>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+//----------------------------------lần 1---------------------------------
+// import Joke from './Joke.js'
+// export default function App(){
+//   return(
+//     <div>
+//       <h1>joke Generator using react and joke API</h1>
+//       <Joke/>
+//     </div>
+//   );
+// }
+
+//-------------------------------lần 2: tự làm --------------------------------------
+import { useState } from "react";
+import "./App.css";
+export default function App() {
+  const [joke, setJoke] = useState("");
+  function fetchApi() {
+    fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+      .then((res) => res.json())
+      .then((data) => setJoke(data.joke));
+  }
   return (
-    <>
-      <h1> Thái Bình Độ</h1>
-      <MyButton />
-    </>
+    <div className="App">
+      <h1>Joke Generator Using React and joke API</h1>
+      <button onClick={fetchApi}>Click to Generator a joke</button>
+      <p>{joke}</p>
+    </div>
   );
 }
-
-
